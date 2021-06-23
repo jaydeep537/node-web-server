679d182b2c9d983a08e9bfa7a96cb7e8858add79
jaydeep537 <solankijaydeep424@gmail.com>
2021-06-23 20:24:27 +0530

    Remove console log

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,3 @@
-console.log("App Runnig");
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const messageOne = document.querySelector("#message-one");
